f1d2a98c105f4d0394addaffe7829240e2d2731c
Sunny Ouyang <ShenOuyang.work@gmail.com>
2018-06-22 20:29:38 -0700

    made assignments cell more polished

--- HSAPP-iOS/SunnyHomeVC/Assignments/AssignmentCollectionCell.swift
+++ HSAPP-iOS/SunnyHomeVC/Assignments/AssignmentCollectionCell.swift
@@ -14,7 +14,15 @@ class AssignmentCollectionCell: UICollectionViewCell {
     var assignment: Assignment?
     
     //MARK: UICOMPONENTS
-    private var containerView = UIView()
+    private var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.AppColors.viewWhite
+        view.layer.shadowColor = UIColor.lightGray.cgColor
+        view.layer.shadowOpacity = 0.3
+        view.layer.shadowOffset = CGSize.zero
+        view.layer.shadowRadius = 7
+        return view
+    }()
     
     private var titleLabel: UILabel = {
         let label = UILabel()
@@ -24,13 +32,15 @@ class AssignmentCollectionCell: UICollectionViewCell {
     
     private var classLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: ".SFUIText-SemiBold", size: 12)
+        label.font = UIFont(name: ".SFUIText-SemiBold", size: 14)
+        label.textColor = UIColor.gray
         return label
     }()
     
     private var dueLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: ".SFUIText-SemiBold", size: 12)
+        label.font = UIFont(name: ".SFUIText-SemiBold", size: 16)
+        label.textColor = UIColor.lightGray
         return label
     }()
     
@@ -68,13 +78,13 @@ class AssignmentCollectionCell: UICollectionViewCell {
         }
         
         classLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(titleLabel.snp.bottom).offset(16)
+            make.top.equalTo(titleLabel.snp.bottom)
             make.left.equalToSuperview().offset(16)
             make.right.equalToSuperview().offset(-16)
         }
         
         dueLabel.snp.makeConstraints { (make) in
-            make.top.equalTo(classLabel.snp.bottom)
+            make.bottom.equalToSuperview().offset(-16)
             make.left.equalToSuperview().offset(16)
             make.right.equalToSuperview().offset(-16)
         }
--- HSAPP-iOS/SunnyHomeVC/SunnyHomeViewController.swift
+++ HSAPP-iOS/SunnyHomeVC/SunnyHomeViewController.swift
@@ -63,13 +63,13 @@ class SunnyHomeViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 1 {
+        if indexPath.section == 0 {
             let cell = homeTableView.dequeueReusableCell(withIdentifier: "MenuTableCell") as! MenuTableCell
             cell.setUp(dataSourceDelegate: self, forRow: indexPath.row)
             guard let collectionView = cell.menuCollectionView else {return cell}
             collectionViews.append(collectionView)
             return cell
-        } else if indexPath.section == 0 {
+        } else if indexPath.section == 1 {
             let cell = homeTableView.dequeueReusableCell(withIdentifier: "AssignmentsTableCell") as! AssignmentsTableCell
             cell.setUp(dataSourceDelegate: self, forRow: indexPath.row)
             guard let collectionView = cell.assignmentsCollectionView else {return cell}
@@ -87,11 +87,11 @@ class SunnyHomeViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        if section == 1 {
+        if section == 0 {
             let view = HeaderHelper.createTasksTitleHeaderView(title: "Featured Lunches:", fontSize: 25, frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 160), color: UIColor.white, bottomOffset: -10)
             return view
-        } else if section == 0 {
-            let view = HeaderHelper.createTasksTitleHeaderView(title: "Assignments Due:", fontSize: 25, frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 160), color: UIColor.white, bottomOffset: -10)
+        } else if section == 1 {
+            let view = HeaderHelper.createTasksTitleHeaderView(title: "Assignments Due:", fontSize: 25, frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 160), color: UIColor.white, bottomOffset: 25)
             return view
         }
         
@@ -108,9 +108,9 @@ extension SunnyHomeViewController: UICollectionViewDelegate, UICollectionViewDat
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == collectionViews[0] {
-            return frenchAssignments.count
-        } else if collectionView == collectionViews[1] {
             return menu.count
+        } else if collectionView == collectionViews[1] {
+            return frenchAssignments.count
         }
         return 0
     }
@@ -118,16 +118,16 @@ extension SunnyHomeViewController: UICollectionViewDelegate, UICollectionViewDat
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if collectionView == collectionViews[0] {
             
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AssignmentsCollectionCell", for: indexPath) as! AssignmentCollectionCell
-            cell.setUp(assignment: frenchAssignments[indexPath.row])
-            return cell
-            
-        } else if collectionView == collectionViews[1] {
-            
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCollectionCell", for: indexPath) as! MenuCollectionCell
             cell.setUp(menuItem: menu[indexPath.row])
             return cell
             
+            
+        } else if collectionView == collectionViews[1] {
+            
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AssignmentsCollectionCell", for: indexPath) as! AssignmentCollectionCell
+            cell.setUp(assignment: frenchAssignments[indexPath.row])
+            return cell
             
         } else {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AssignmentsCollectionCell", for: indexPath) as! AssignmentCollectionCell
@@ -137,6 +137,11 @@ extension SunnyHomeViewController: UICollectionViewDelegate, UICollectionViewDat
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == collectionViews[0] {
+            return CGSize(width: self.view.bounds.width * 0.675, height: 110)
+        } else if collectionView == collectionViews[1] {
+            return CGSize(width: self.view.bounds.width * 0.8, height: 110)
+        }
         return CGSize(width: self.view.bounds.width * 0.9, height: 110)
     }
     
